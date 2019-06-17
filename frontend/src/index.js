334b5e3bae35079d6866d4da61f584937ad9385e
romazrau <b04208017@ntu.edu.tw>
2019-06-17 18:58:29 +0800

    redux  init

---
+++ frontend/src/index.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { render } from "react-dom";
+import { Provider } from "react-redux";
+import store from "./js/store/index";
+import App from ".//js/components/App.jsx";
+
+import * as serviceWorker from './serviceWorker';
+
+render(
+  <Provider store={store}>
+    <App ></App>
+  </Provider>,
+
+  document.getElementById("root")
+);
+
+
+serviceWorker.unregister();
